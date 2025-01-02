1bbb6b4c9156f23dbffac9d78de1156c2cff328d
aliimron26 <147901793+aliimron26@users.noreply.github.com>
2025-01-02 17:48:09 +0700

    Update sidebar-admin.php

--- Tel-U_Looks/Layouts/sidebar-admin.php
+++ Tel-U_Looks/Layouts/sidebar-admin.php
@@ -18,7 +18,7 @@
       <a href="../admin/list_recomendation.php"><i class="bi bi-list-ul"></i> <span>Daftar Rekomendasi</span></a>
       <a href="../admin/Add_recomendation_admin.php"><i class="bi bi-plus-square"></i> <span>Tambah Rekomendasi</span></a>
       <a href="../admin/verify_recomendation_user.php"><i class="bi bi-hourglass-split"></i> <span>Rekomendasi Pending</span></a>
-      <a href="../admin/list_users.php" class="active"><i class="bi bi-people"></i> <span>Daftar Pengguna</span></a>
+      <a href="../admin/list_users.php"><i class="bi bi-people"></i> <span>Daftar Pengguna</span></a>
       <a href="../artikel/create_article.php"><i class="bi bi-file-earmark-plus"></i> <span>Tambah Artikel</span></a>
       <a href="../artikel/manage_articles.php"><i class="bi bi-folder"></i> <span>Kelola Artikel</span></a>
     </div>
